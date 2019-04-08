8bf5bf9576c9dc319cc0eadd2b8551368a4a670a
Elvina <elvina.ri@bk.ru>
2019-04-09 00:01:24 +0400

    модуль 5

--- src/point/index.js
+++ src/point/index.js
@@ -1,9 +1,11 @@
 import {getIcon} from './icons';
 import {getOffers} from './offers';
 import {getSchedule} from './schedule';
+import {getImages} from './images';
 
 export {
   getIcon,
   getOffers,
   getSchedule,
+  getImages
 };
